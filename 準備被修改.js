00470666ab4ef6be291f61e111b465d0823228d2
imall <imall841110@gmail.com>
2023-07-06 16:54:29 +0800

    abc
    abcdefg台灣有個阿里山
    
    cat

--- 準備被修改.js
+++ 準備被修改.js
@@ -2,9 +2,4 @@ function addNumbers(a, b) {
     return a + b;
   }
   
-  const num1 = 5;
-  const num2 = 10;
-  const sum = addNumbers(num1, num2);
-  
-  console.log(`全部是 ${num1} and ${num2} is ${sum}.`);
-  
+  c
